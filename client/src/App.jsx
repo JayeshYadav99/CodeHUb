84bffe96d5b8b5737847489e1bcea9f72e849c9e
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2023-05-28 18:52:58 +0530

    Github deleted

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ import Navbar from "./Pages/Navbar";
 import Hero from "./Pages/Hero";
 import Footer2 from "./Pages/Footer2";
 import Repo from "./Pages/Repo";
-import Github from "./Pages/Github";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CodeHome from "./Pages/CodeHome";
 import CertificationCourses from "./Pages/Learn/CertificationCourses";
@@ -31,7 +31,7 @@ const App = () => {
           <Routes>
             <Route exact path="/" element={<CodeHome />}></Route>
             <Route exact path="/Repo" element={<Repo />}></Route>
-            <Route exact path="/Github" element={<Github />}></Route>
+
             <Route exact path="/Problem" element={<LanguageTracks />}></Route>
             <Route exact path="/JAVA" element={<Java />}></Route>
             <Route exact path="/Python" element={<Python />}></Route>
